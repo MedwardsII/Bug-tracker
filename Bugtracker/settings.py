9b2dbf3f2f9e217b83ad3a6a8266d173ae2c0f67
Michael N. Edwards II <mnedwards118@gmail.com>
2022-02-16 00:43:29 -0800

    Added session expiration

--- Bugtracker/settings.py
+++ Bugtracker/settings.py
@@ -18,6 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# session expiration
+SESSION_COOKIE_AGE = 1800
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
