632c41599bc70ab958957aec4b2ef26f3b44aaa3
Michael N. Edwards II <mnedwards118@gmail.com>
2022-04-11 22:05:32 -0700

    Created tests for data models.

--- Bugtracker/settings.py
+++ Bugtracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'unsecure-0zCVdHXtqbYBA873y9kJX1tDmWznSAAaereCwS0kb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- bugs/tests.py
+++ bugs/tests.py
@@ -1,3 +1,78 @@
 from django.test import TestCase
+from django.db import models
+from .models import Project, Ticket, TicketComment
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestProjectModel(TestCase):
+    '''Test the project model.'''
+    def setUp(self) -> None:
+        self.project = Project(
+            title='Project Name'
+        )
+    def test_create_project(self):
+        self.assertIsInstance(self.project, Project)
+    def test_str_representation(self):
+        self.assertEquals(str(self.project), 'Project Name')
+
+class TestTicketModel(TestCase):
+    '''Test the ticket model.'''
+    def setUp(self) -> None:
+        self.project = Project(
+            title='Ticket Name'
+        )
+        self.user = User(
+            username='Username',
+            password='password'
+        )
+        self.ticket = Ticket(
+            description='Ticket Description',
+            is_resolved=False,
+            priority='Urgent',
+            created_on=models.DateTimeField(
+                auto_now_add=True,
+            ),
+            last_updated=models.DateTimeField(
+                auto_now_add=True,
+            ),
+            project=self.project,
+            creator=self.user,
+        )
+    def test_create_ticket(self):
+        self.assertIsInstance(self.ticket, Ticket)
+    def test_str_representation(self):
+        self.assertEquals(str(self.ticket), 'Ticket Description')
+
+class TestTicketCommentModel(TestCase):
+    '''Test the ticket comment model.'''
+    def setUp(self) -> None:
+        self.project = Project(
+            title='Ticket Name'
+        )
+        self.user = User(
+            username='Username',
+            password='password'
+        )
+        self.ticket = Ticket(
+            description='Ticket Description',
+            is_resolved=False,
+            priority='Urgent',
+            created_on=models.DateTimeField(
+                auto_now_add=True,
+            ),
+            last_updated=models.DateTimeField(
+                auto_now_add=True,
+            ),
+            project=self.project,
+            creator=self.user,
+        )
+        self.ticket_comment = TicketComment(
+            comment='This is a comment.',
+            creator=self.user,
+            ticket=self.ticket,
+            created_on=models.DateTimeField(
+                auto_now_add=True,
+            )
+        )
+    def test_create_ticketcomment(self):
+        self.assertIsInstance(self.ticket_comment, TicketComment)
